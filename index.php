c612f06863dcaf208a23159e2743cefa81916512
Denny Rakhmad Widi Ashari <dennyrwa@gmail.com>
2021-02-27 12:40:04 +0700

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	//membuat metode redirect dengan kode 301
-	header(“location: school.nasiwebhost.com/index.html”, true, 301);
-	//membuat kode di bawah header tidak diproses oleh website sehingga lebih aman
-	exit();
- ?>
